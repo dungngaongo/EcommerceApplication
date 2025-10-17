dc7bf4e071c6ecb8ec33e92595a0f9e1ffb58838
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-10-17 15:06:42 +0700

    create order

--- src/main/java/com/app/ecom_application/model/OrderItem.java
+++ src/main/java/com/app/ecom_application/model/OrderItem.java
@@ -1,6 +1,7 @@
 package com.app.ecom_application.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -8,6 +9,7 @@ import java.math.BigDecimal;
 
 @Entity
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class OrderItem {
     @Id
---
+++ src/main/java/com/app/ecom_application/service/OrderService.java
@@ -0,0 +1,80 @@
+package com.app.ecom_application.service;
+
+import com.app.ecom_application.dto.OrderItemDTO;
+import com.app.ecom_application.dto.OrderResponse;
+import com.app.ecom_application.model.*;
+import com.app.ecom_application.repository.OrderRepository;
+import com.app.ecom_application.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final CartService cartService;
+    private final UserRepository userRepository;
+    private final OrderRepository orderRepository;
+
+    public Optional<OrderResponse> createOrder(String userId) {
+        List<CartItem> cartItems = cartService.getCart(userId);
+        if(cartItems.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Optional<User> userOptional = userRepository.findById(Long.valueOf(userId));
+        if(userOptional.isEmpty()) {
+            return Optional.empty();
+        }
+        User user = userOptional.get();
+
+        BigDecimal totalPrice = cartItems.stream()
+                .map(CartItem::getPrice)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        Order order = new Order();
+        order.setUser(user);
+        order.setStatus(OrderStatus.CONFIRMED);
+        order.setTotalAmount(totalPrice);
+
+        List<OrderItem> orderItems = cartItems.stream()
+                .map(item -> new OrderItem(
+                        null,
+                        item.getProduct(),
+                        item.getQuantity(),
+                        item.getPrice(),
+                        order
+                ))
+                .toList();
+
+        order.setItems(orderItems);
+        Order savedOrder = orderRepository.save(order);
+
+        cartService.clearCart(userId);
+        
+        return Optional.of(mapToOrderResponse(savedOrder));
+    }
+
+    private OrderResponse mapToOrderResponse(Order order) {
+        return new OrderResponse(
+                order.getId(),
+                order.getTotalAmount(),
+                order.getStatus(),
+                order.getItems().stream()
+                        .map(orderItem -> new OrderItemDTO(
+                                orderItem.getId(),
+                                orderItem.getProduct().getId(),
+                                orderItem.getQuantity(),
+                                orderItem.getPrice(),
+                                orderItem.getPrice().multiply(new BigDecimal(orderItem.getQuantity()))
+                        ))
+                        .toList(),
+                order.getCreatedAt()
+        );
+    }
+}
