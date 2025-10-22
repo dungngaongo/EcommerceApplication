cab97cc0d0ecfe8191b8aba3712323d603f10556
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-10-22 12:56:03 +0700

    mongodb cloud for user and postgres cloud for product

--- user/src/main/java/com/ecommerce/user/services/UserService.java
+++ user/src/main/java/com/ecommerce/user/services/UserService.java
@@ -46,12 +46,12 @@ public class UserService {
         }
     }
 
-    public Optional<UserResponse> fetchUser(Long id) {
+    public Optional<UserResponse> fetchUser(String id) {
         return userRepository.findById(id)
                 .map(this::mapToUserResponse);
     }
 
-    public boolean updateUser(Long id, UserRequest updatedUserRequest) {
+    public boolean updateUser(String id, UserRequest updatedUserRequest) {
         return userRepository.findById(id)
                 .map(existingUser -> {
                     updateUserFromRequest(existingUser, updatedUserRequest);
