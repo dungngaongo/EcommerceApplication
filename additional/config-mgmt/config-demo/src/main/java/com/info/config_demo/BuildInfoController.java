4f1f67c4ae19b3b950dfb4fb7b1892bc67f80713
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-10-28 02:20:16 +0700

    encrypt

--- additional/config-mgmt/config-demo/src/main/java/com/info/config_demo/BuildInfoController.java
+++ additional/config-mgmt/config-demo/src/main/java/com/info/config_demo/BuildInfoController.java
@@ -2,16 +2,28 @@ package com.info.config_demo;
 
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@AllArgsConstructor
+//@AllArgsConstructor
+@RefreshScope
 public class BuildInfoController {
-    private final BuildInfo buildInfo;
+//    private final BuildInfo buildInfo;
+
+    @Value("${build.id:default}")
+    private String buildId;
+
+    @Value("${build.version:default}")
+    private String buildVersion;
+
+    @Value("${build.name:default}")
+    private String buildName;
 
     @GetMapping("/build-info")
     public String getBuildInfo() {
-        return "Build Id: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+//        return "Build Id: " + buildInfo.getId() + ", Version: " + buildInfo.getVersion() + ", Name: " + buildInfo.getName();
+        return "Build Id: " + buildId + ", Version: " + buildVersion + ", Name: " + buildName;
     }
 }
