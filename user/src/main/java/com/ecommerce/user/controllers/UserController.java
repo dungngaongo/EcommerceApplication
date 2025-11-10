2022fdb090254a5e3463127b1a3f75e3d80d8b35
Nguyen Tan Dung <09tandungnguyen@gmail.com>
2025-11-11 00:55:21 +0700

    add logging

--- user/src/main/java/com/ecommerce/user/controllers/UserController.java
+++ user/src/main/java/com/ecommerce/user/controllers/UserController.java
@@ -4,6 +4,9 @@ import com.ecommerce.user.dto.UserRequest;
 import com.ecommerce.user.dto.UserResponse;
 import com.ecommerce.user.services.UserService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +16,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
+@Slf4j
 public class UserController {
     private final UserService userService;
 
@@ -24,6 +28,14 @@ public class UserController {
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUser(@PathVariable String id) {
+        log.info("Request received for user: {}", id);
+
+        log.trace("This is TRACE level - Very detailed logs");
+        log.debug("This is DEBUG level - Used for development debugging");
+        log.info("This is INFO level - General system information");
+        log.warn("This is WARN level - Something might be wrong");
+        log.error("This is ERROR level - Something failed");
+
         return userService.fetchUser(id)
                 .map(ResponseEntity::ok)
                 .orElseGet(() -> ResponseEntity.notFound().build());
